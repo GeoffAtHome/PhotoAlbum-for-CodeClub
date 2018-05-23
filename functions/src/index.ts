d91e0b9af540eb31249d1efb9d2f4a0da35fae15
GeoffAtHome <geoff@soord.org.uk>
2018-05-23 18:39:28 +0100

    Update firebase functions.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -52,44 +52,7 @@ exports.onFileChange = functions.storage.object('/upload/').onFinalize(event => 
         // Once the image has been uploaded delete the local files to free up disk space.
         fs.unlinkSync(srcFilePath);
         fs.unlinkSync(resizedFilePath);
-
-        const promises = [];
-
-        const config = {
-            action: 'read',
-            expires: '03-01-2500',
-        };
-        for (const filename in files) {
-            const file = bucket.file(filename);
-            const p = file.getSignedUrl(config);
-            promises.push(p)
-        }
-        return Promise.all(promises);
-    }).then((results) => {
-        const links = [];
-        for (const r of results) {
-            links.push(r);
-        }
-        const albumListRef = admin.database().ref('images');
-        albumListRef.once("value", (snapshot) => {
-            console.log("Updating snapshot");
-            console.log(snapshot.val());
-            if (snapshot.val() !== null) {
-                snapshot.val().items.push({ title: "", description: "", links: links })
-                return albumListRef.update(snapshot);
-            }
-            const document = { description: "", title: "", items: [] };
-            document.items.push({ title: "", description: "", links: links })
-            return albumListRef.update(document);
-
-        }, (errorObject) => {
-            console.log("The read failed: " + errorObject.code);
-            const document = { description: "", title: "", items: [] };
-            document.items.push({ title: "", description: "", links: links })
-            return albumListRef.update(document);
-        });
-
-    });
+    })
 });
 
 function ResizeImage(srcFilePath, resizedFilePath, destBucket, metadata, destination, size) {
